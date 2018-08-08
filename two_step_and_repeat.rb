567a5224e99dc12ef04a7064608ed4e5356fdac5
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-08 15:25:12 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -13,6 +13,14 @@ end
   first_steps
 
 def a_few_more_steps
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
   puts "Right foot steps back"
   sleep(0.5)
   puts "Left foot crosses over right"
