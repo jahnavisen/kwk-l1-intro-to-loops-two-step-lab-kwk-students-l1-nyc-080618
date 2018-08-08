3d7bcf4ef0e7a9d52b1204ff9c4bd02578a5736a
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-08 15:17:33 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,29 +1,29 @@
-# def first_steps
-#   puts "Right foot back"
-#   sleep(0.5)
-#   puts "Left foot back"
-#   sleep(0.5)
-#   puts "Right foot back"
-#   sleep(0.5)
-#   puts "Stop"
-#   sleep(1)
-#   # Write a loop that outputs "Right foot back", "Left foot back",
-#   # "Right foot back", and "Stop", with sleep after each
-# end
-#   first_steps
+def first_steps
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
+  # Write a loop that outputs "Right foot back", "Left foot back",
+  # "Right foot back", and "Stop", with sleep after each
+end
+  first_steps
 
-# def a_few_more_steps
-#   puts "Right foot steps back"
-#   sleep(0.5)
-#   puts "Left foot crosses over right"
-#   sleep(0.5)
-#   puts "Right foot steps right"
-#   sleep(0.5)
-#   puts "Turn"
-#   sleep(1)
-#   # Write a loop that outputs the first two sets of steps in the Two-Step
-# end
-#   a_few_more_steps
+def a_few_more_steps
+  puts "Right foot steps back"
+  sleep(0.5)
+  puts "Left foot crosses over right"
+  sleep(0.5)
+  puts "Right foot steps right"
+  sleep(0.5)
+  puts "Turn"
+  sleep(1)
+  # Write a loop that outputs the first two sets of steps in the Two-Step
+end
+  a_few_more_steps
 
 def how_many_steps?
     steps = 0
